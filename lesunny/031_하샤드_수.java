962a31c8f1d24d1ff1fea48db310209c61bd49a7
Lee Dasom <33215628+LeSunny@users.noreply.github.com>
2020-05-24 01:07:42 +0900

    Create 031_하샤드_수.java

---
+++ lesunny/031_하샤드_수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        int sum = 0;
+        double dx = x;
+
+        while(x!= 0){
+            sum += (x%10);
+            x /= 10;
+        }
+        
+        double result = dx%(double)sum;
+        
+        if(result != 0) answer = false;
+        
+        return answer;
+    }
+}
