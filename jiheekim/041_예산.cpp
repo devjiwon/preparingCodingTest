8066f159377494c3c2c85d74fcb8cff61a30cc46
kkamjee <kpjihee@naver.com>
2020-07-09 02:42:45 +0900

    Update 041_예산.cpp

--- jiheekim/041_예산.cpp
+++ jiheekim/041_예산.cpp
@@ -8,14 +8,15 @@ using namespace std;
 
 int solution(vector<int> d, int budget) {
 int sum=0;
-    
+    int temp=budget;
  sort(d.begin(), d.end());
 
     for (int i=0; i<d.size();i++){
-        budget=budget-d[i];
-    sum=sum+d[i];
-        if(budget<sum)
-                return d.size();
+       budget=budget-d[i];
+       sum=sum+d[i];
+  
+        if(sum>temp)
+                return i;
     }
 
 }
