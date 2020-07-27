aacc7e6489a304fc83e87b016efcd4ac9ea0ca15
Lee Dasom <33215628+LeSunny@users.noreply.github.com>
2020-07-27 13:31:05 +0900

    Update 037 체육복.cpp

--- jiwon/037 체육복.cpp
+++ jiwon/037 체육복.cpp
@@ -1,37 +1,50 @@
-#include <string>
-#include <vector>
-
-using namespace std;
-
-int solution(int n, vector<int> lost, vector<int> reserve) {
-    int answer = n - lost.size();
-    
-    for(int i=0;i<reserve.size();i++){
-        for(int j=0;j<lost.size();j++){
-            if(reserve[i] == lost[j]){
-                // reserve[i] = 80;
-                // lost[j] = 90;
-                lost.erase(lost.begin()+(j--));
-                reserve.erase(reserve.begin()+(i--));
-                answer++;
-                break;
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        int[] flags = new int[n+1]; //0~n
+        
+        //초기화
+        for(int i=1; i<=n; i++)
+            flags[i] = 1;
+        
+        //lost와 reseve가 같을 때
+        for(int i=0; i<lost.length; i++){
+            for(int j=0; j<reserve.length; j++){
+                if(lost[i]==reserve[j]){
+                    flags[lost[i]] = -2;
+                    answer++;
+                    break;
+                }                    
             }
         }
-    }
-    
-    for(int i=0;i<reserve.size();i++){
-        for(int j=0;j<lost.size();j++){
-            if(reserve[i] == lost[j] - 1 || reserve[i] == lost[j] + 1){
-                answer++;
-                // reserve[i] = 100;
-                // lost[j] = 50;
-                lost.erase(lost.begin()+(j--));
-                reserve.erase(reserve.begin()+(i--));
-                break;
+        
+        //빼기
+        for(int i=0; i<lost.length; i++)
+            flags[lost[i]] -= 1;
+        
+        //나눠주기
+        for(int i=0; i<reserve.length; i++){    
+            flags[reserve[i]] += 1;
+            
+            if(flags[reserve[i]]>1 && reserve[i]-1>0 && flags[reserve[i]-1] == 0){
+                flags[reserve[i]-1] += 1;
+                flags[reserve[i]] -= 1;
+            }else if(flags[reserve[i]]>1 && reserve[i]+1<n+1 && flags[reserve[i]+1] == 0){
+                flags[reserve[i]+1] += 1;
+                flags[reserve[i]] -= 1;
             }
         }
+        
+        
+        
+        for(int i=1; i<n+1; i++){
+            System.out.print(flags[i]+" ");
+            if(flags[i]>0)
+                answer++;
+        }    
+        
+        
+        
+        return answer;
     }
-    
-    
-    return answer;
 }
