54a7d1cd94c10168cf8f880dd7985dc4a283741b
Lee Dasom <33215628+LeSunny@users.noreply.github.com>
2020-07-27 13:14:14 +0900

    Update 037_체육복(오답).java

--- lesunny/037_체육복(오답).java
+++ lesunny/037_체육복(오답).java
@@ -1,11 +1,11 @@
-//테스트케이스 3, 5, 7, 8, 10, 12 실패
+//테스트케이스 5,7 실패
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
         int[] flags = new int[n+1]; //0~n
         
         //초기화
-        for(int i=1; i<n+1; i++)
+        for(int i=1; i<=n; i++)
             flags[i] = 1;
         
         for(int i=0; i<lost.length; i++)
@@ -18,15 +18,18 @@ class Solution {
             if(flags[reserve[i]]>1 && reserve[i]-1>0 && flags[reserve[i]-1] == 0){
                 flags[reserve[i]-1] += 1;
                 flags[reserve[i]] -= 1;
-            }else if(flags[reserve[i]]>1 && reserve[i]+1>n && flags[reserve[i]+1] == 0){
+            }else if(flags[reserve[i]]>1 && reserve[i]+1<n+1 && flags[reserve[i]+1] == 0){
                 flags[reserve[i]+1] += 1;
                 flags[reserve[i]] -= 1;
             }
         }
         
+        
+        
         for(int i=1; i<n+1; i++){
-            System.out.println(flags[i]);
-            if(flags[i]>0) answer++;
+            System.out.print(flags[i]+" ");
+            if(flags[i]>0)
+                answer++;
         }    
         
         
