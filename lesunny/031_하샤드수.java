2d40805d755406aa708e4945b8cd6fe36a93ac72
Lee Dasom <33215628+LeSunny@users.noreply.github.com>
2020-07-28 18:20:45 +0900

    Update 031_하샤드수.java

--- lesunny/031_하샤드수.java
+++ lesunny/031_하샤드수.java
@@ -2,16 +2,17 @@ class Solution {
     public boolean solution(int x) {
         boolean answer = true;
         int sum = 0;
-        double dx = x;
+        int tmpX = x;
 
-        while(x!= 0){
-            sum += (x%10);
-            x /= 10;
+        while(tmpX != 0){
+            sum += (tmpX%10);
+            tmpX /= 10;
         }
         
-        double result = dx%sum;
+        int result = x%sum;
         
-        if(result != 0) answer = false;
+        if(result != 0)
+            answer = false;
         
         return answer;
     }
