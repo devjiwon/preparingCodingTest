90db6c2a7a15656e8c77888716007a9c483ac763
lesunny <rhdtha01@naver.com>
2020-08-03 13:07:28 +0900

    Update 056_전화번호목록.java

--- lesunny/056_전화번호목록.java
+++ lesunny/056_전화번호목록.java
@@ -1,13 +1,18 @@
 class Solution {
     public boolean solution(String[] phone_book) {
-        
+        boolean answer = true;
+
         for(int i=0; i<phone_book.length; i++){          
             for(int j=0; j<phone_book.length ; j++){
                 if(i!=j){
-                    if(phone_book[j].contains(phone_book[i]) && phone_book[j].substring(0,phone_book[i].length()).equals(phone_book[i]))
-                        return false;
-                    else if(phone_book[i].contains(phone_book[j]) && phone_book[i].substring(0,phone_book[j].length()).equals(phone_book[j]))
-                        return false;
+                    if(phone_book[j].contains(phone_book[i]) && phone_book[j].substring(0,phone_book[i].length()).equals(phone_book[i])){
+                        answer = false;
+			return answer;
+
+			}
+                    else if(phone_book[i].contains(phone_book[j]) && phone_book[i].substring(0,phone_book[j].length()).equals(phone_book[j])){
+                        return answer;
+			}
                 }
             }
         }
