dba2cc3a34a183fdb452505d3caf6d2c2d2dd3c7
lesunny <rhdtha01@naver.com>
2020-08-03 13:11:07 +0900

    Update 056_전화번호목록.java

--- lesunny/056_전화번호목록.java
+++ lesunny/056_전화번호목록.java
@@ -7,16 +7,15 @@ class Solution {
                 if(i!=j){
                     if(phone_book[j].contains(phone_book[i]) && phone_book[j].substring(0,phone_book[i].length()).equals(phone_book[i])){
                         answer = false;
-			return answer;
-
-			}
+		}
                     else if(phone_book[i].contains(phone_book[j]) && phone_book[i].substring(0,phone_book[j].length()).equals(phone_book[j])){
-                        return answer;
+                        answer = false;
 			}
+			return answer;
                 }
             }
         }
         
-        return true;
+        return answer;
     }
 }
