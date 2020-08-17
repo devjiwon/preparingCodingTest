f0683a152ff73e4bea6ce4d2770dd0dc0b442cf2
Sohyeon Park <sohyun1018@gmail.com>
2020-08-17 11:59:54 +0900

    Create Update 061_프린터.cpp

---
+++ Sohyeon/Update 061_프린터.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int solution(vector<int> priorities, int location) {
+    int answer = 0;
+   queue<pair<int, int> > q;
+   priority_queue<int> pq;
+
+   for(int i=0;i<priorities.size();i++){
+       q.push(make_pair(i,priorities[i]));
+       pq.push(priorities[i]);
+   }
+   while(!q.empty()){
+       if(q.front().second == pq.top()){
+           if(q.front().first == location){
+               return answer+1;
+           } else {
+               answer++;
+               q.pop();
+               pq.pop();
+           }
+       } else {
+           q.push(q.front());
+           q.pop();
+       }
+
+   }
+    return answer;
+}
