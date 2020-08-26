1eadf8316d6133c0040961f0c01cde240e0be0c1
Lee Dasom <33215628+LeSunny@users.noreply.github.com>
2020-08-26 14:45:13 +0900

    Update 073_분해합.java

--- lesunny/073_분해합.java
+++ lesunny/073_분해합.java
@@ -5,22 +5,23 @@ public class Main{
 		Scanner in = new Scanner(System.in);
 		int decomposition = in.nextInt();
 		int size = String.valueOf(decomposition).length();
-		
-        for(int i=decomposition-(9*size); i<decomposition; i++) { //constructor i
+	
+		for(int i=decomposition-(9*size); i<decomposition; i++) { //constructor i
 			int tmpDecomposition = i;
 			int tmp = i;
 			while(tmp>0) {
 				tmpDecomposition += tmp%10;
 				tmp/=10;
-			}
+			}	
 			
 			if(tmpDecomposition == decomposition) {
-                System.out.println(i);
+				System.out.println(i);
 				return;
-			}else if(i==decomposition-1){
-                System.out.println(0);
-                return;
-            }
+			}
+			else if(i==decomposition-1){
+				System.out.println(0);
+			return;
+			}
 		}		
 	}
 }
